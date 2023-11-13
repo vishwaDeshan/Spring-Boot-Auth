d679b9e647ae7f32763bcfafacd1f65b095e7ce0
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-11-13 19:22:26 +0530

    Update the schema.

--- auth/src/main/java/com/sringboot/auth/controller/UserController.java
+++ auth/src/main/java/com/sringboot/auth/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sringboot.auth.controller;
 
 import com.sringboot.auth.model.User;
 import com.sringboot.auth.service.UserService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,11 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostConstruct
+    public void initRolesAndUsers(){
+        userService.initRolesAndUser();
+    }
     @PostMapping({"/registerNewUser"})
     public User registerNewUser(@RequestBody User user){
        return userService.registerNewUser(user);
--- auth/src/main/java/com/sringboot/auth/service/UserService.java
+++ auth/src/main/java/com/sringboot/auth/service/UserService.java
@@ -1,16 +1,55 @@
 package com.sringboot.auth.service;
 
+import com.sringboot.auth.model.Role;
 import com.sringboot.auth.model.User;
+import com.sringboot.auth.repository.RoleRepository;
 import com.sringboot.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
     public User registerNewUser(User user){
         return userRepository.save(user);
+    }
+
+    public void initRolesAndUser(){
+        Role adminRole=new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("Admin Role");
+        roleRepository.save(adminRole);
+
+        Role userRole=new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("Default Role for newly created record");
+        roleRepository.save(userRole);
+
+        User adminUser=new User();
+        adminUser.setUserFirstName("Admin");
+        adminUser.setUserLastName("Admin");
+        adminUser.setUserName("Admin123");
+        adminUser.setUserPassword("Admin@pass");
+        Set<Role> adminRoles =new HashSet<>();
+        adminRoles.add(adminRole);
+        userRepository.save(adminUser);
+
+        User user=new User();
+        user.setUserFirstName("Vishwa");
+        user.setUserLastName("Deshan");
+        user.setUserName("Vishwa123");
+        user.setUserPassword("Vishwa@pass");
+        Set<Role> userRoles =new HashSet<>();
+        userRoles.add(userRole);
+        userRepository.save(user);
     }
 }
